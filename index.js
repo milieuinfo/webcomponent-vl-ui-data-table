80ce06f78e8624d4fa530421730ad4a59105cb22
Pascal Riquier <pascal.riquier@vlaanderen.be>
2020-03-16 08:46:14 +0100

    fix pad

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = { 
     Test: {
-        VlDataTable: require('./test/e2e/components/vl-data-table.js').VlDataTable
+        VlDataTable: require('./test/e2e/components/vl-data-table.js')
     }
 }
