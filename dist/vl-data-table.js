f42c8080dd5bfb9d0c615bb56a8b3db789fab9c5
Tom Coemans <tom.coemans@gmail.com>
2020-06-03 17:19:08 +0200

    ESlint

--- dist/vl-data-table.js
+++ dist/vl-data-table.js
@@ -1,11 +1,13 @@
-import { NativeVlElement, define } from '/node_modules/vl-ui-core/dist/vl-core.js';
+import {nativeVlElement} from '/node_modules/vl-ui-core/dist/vl-core.js';
+import {define} from '/node_modules/vl-ui-core/dist/vl-core.js';
 
 /**
  * VlDataTable
  * @class
  * @classdesc Gebruik een data table om op een gestructureerde manier (grote hoeveelheden) relationele data te tonen.
  *
- * @extends NativeVlElement
+ * @extends HTMLTableElement
+ * @mixin nativeVlElement
  *
  * @property {boolean} hover - Attribuut wordt gebruikt om een rij te highlighten waneer de gebruiker erover hovert met muiscursor.
  * @property {boolean} matrix - Attribuut wordt gebruikt om data in 2 dimensies te tonen. Zowel de rijen als de kolommen krijgen een titel. Deze titels worden gescheiden door een dikke lijn.
@@ -14,12 +16,12 @@ import { NativeVlElement, define } from '/node_modules/vl-ui-core/dist/vl-core.j
  * @property {boolean} collaped-m - Vanaf een medium schermgrootte zullen de cellen van elke rij onder elkaar ipv naast elkaar getoond worden.
  * @property {boolean} collaped-s - Vanaf een small schermgrootte zullen de cellen van elke rij onder elkaar ipv naast elkaar getoond worden.
  * @property {boolean} collaped-xs - Vanaf een extra small schermgrootte zullen de cellen van elke rij onder elkaar ipv naast elkaar getoond worden.
- * 
+ *
  * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-data-table/releases/latest|Release notes}
  * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-data-table/issues|Issues}
  * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-data-table.html|Demo}
  */
-export class VlDataTable extends NativeVlElement(HTMLTableElement) {
+export class VlDataTable extends nativeVlElement(HTMLTableElement) {
   static get _observedAttributes() {
     return [];
   }
